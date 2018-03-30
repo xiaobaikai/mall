56fbe6215fb96326bae7e67ea905021db7d5b743
K9WTHOZWIR0R5SW\Administrator <1181141792@qq.com>
2018-03-30 11:08:46 +0800

    店铺搜索列表

---
+++ src/page/mall/store/StoreSearch.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>店铺搜索页</div>
+</template>
+<script>
+  export default {
+  	data(){
+  		return{
+  			
+  			
+      }
+    }
+  }
+</script>
+<style lang="stylus" scoped="true">
+
+</style>
