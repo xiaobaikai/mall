b59e9e2c52b174a6ede5a3750b9daa9ff02007ee
K9WTHOZWIR0R5SW\Administrator <1181141792@qq.com>
2018-12-12 14:35:31 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -38,8 +38,6 @@ Vue.prototype.mallToken = mallToken;
 Vue.prototype.isApp = isApp;
 Vue.prototype.mallType = mallType;
 
-import FastClick from 'fastclick'
-FastClick.attach(document.body);
 
 //  提示框
 import {Alert, Confirm, Prompt, Toast} from "wc-messagebox";
@@ -50,8 +48,7 @@ Vue.use(Confirm);
 Vue.use(Toast);
 Vue.use(Alert);
 Vue.use(wcSwiper);
-<<<<<<< HEAD
-=======
+
 
 //日历插件
 import Calendar from 'vue-mobile-calendar'
@@ -60,7 +57,7 @@ Vue.use(Calendar);
 import FastClick from 'fastclick';
 FastClick.attach(document.body)
 
->>>>>>> e30acc74468b3c3bbbe04e3c42a2fa0fa6fcc083
+
 window.eventBus = new Vue();
 /* eslint-disable no-new */
 new Vue({
